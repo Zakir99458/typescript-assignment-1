cf24d11c4405dfedeb89a60e3461c521bcf336d6
Mohammad Z. Hossain (Nokia) <mohammad.z.hossain@nokia.com>
2024-05-08 12:47:14 +0300

    problem-1

--- src/problem-1.js
+++ src/problem-1.js
@@ -1,2 +1,12 @@
 {
+    // Question: Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+    // Define arrow function for typeScript
+    var repeatString = function (str, num) {
+        var result = "";
+        for (var i = 0; i < num; i++) {
+            result += str;
+        }
+        return result;
+    };
+    console.log(repeatString("Hello", 5));
 }
--- src/problem-1.ts
+++ src/problem-1.ts
@@ -1,4 +1,17 @@
-{
 
+{
+    // Question: Design a TypeScript function repeatString that takes two parameters: a string and a number. The function should return a new string that repeats the input string the specified number of times.
+
+// Define arrow function for typeScript
+const repeatString = (str: string, num: number): string => {
+    let result = "";
+    for(let i=0; i<num; i++){
+        result += str;
+    }
     
+    return result;
+}
+
+console.log(repeatString("Hello", 5));
+
 }
